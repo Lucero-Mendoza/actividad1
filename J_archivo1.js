356478b07ab4a6613a3a0fbfeae97954c7305e9b
“Lucero-Mendoza” <lucero03mendozag@gmail.com>
2021-05-18 21:15:43 -0500

    Modificar el mismo archivo

--- J_archivo1.js
+++ J_archivo1.js
@@ -1,2 +1,9 @@
 let x = 2 < 3 ? '2 es menor que 3' : '2 es mayor que 3';
 console.log(x);
+
+let examen = {
+    titulo: "Primer parcial",
+    puntosObtenidos : 40,
+    puntosTotales : 100,
+};
+console.log(examen);
